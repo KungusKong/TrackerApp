e1af5dc3233e1635d0b672f0539db1aa4d8ec8a9
KungusKong <thomasdawsondev@gmail.com>
2021-10-02 16:54:07 -0500

    Commit

---
+++ src/app/models/monster.model.ts
@@ -0,0 +1,7 @@
+export class Monster {
+    name: string = "";
+    url: string = "none";
+    hp: number = 20;
+    iniative: number = 0;
+
+}
---
+++ src/app/models/tracker.model.ts
@@ -0,0 +1,6 @@
+import {Monster} from 'src/app/models/monster.model';
+export class Tracker {
+    id: string = "";
+    items: Monster[] =[];
+
+}
